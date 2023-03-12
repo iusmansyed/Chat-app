cc7d997ce69a1e01f00418d45bf115a6bbb5eac8
SyedMuhammadUsman <syedmuhammadusman015@gmail.com>
2023-03-12 12:25:59 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chats from "./Chats/chats";
+const socket = io.connect("http://localhost:3001");
 function App() {
+  const[name ,setName]=useState("")
+  const[room ,setRoom]=useState("")
+  const JoinRoom = ()=>{
+if(name !== "" && room !==""){
+  socket.emit("join_room",room)
+}
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h3>join room</h3>
+        <input
+          placeholder="Enter User Name"
+          onChange={(event) => setName(event.target.value)}
+        />
+        <input
+          placeholder="Enter id "
+          onChange={(event) => setRoom(event.target.value)}
+        />
+        <button onClick={JoinRoom}>Submit</button>
+      </div>
+      <Chats socket={socket} name={name} room={room}/>
     </div>
   );
 }
---
+++ src/Chats/Chats.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Chats = ({socket ,room ,name}) => {
+  return (
+    <div>Chats</div>
+  )
+}
+
+export default Chats
